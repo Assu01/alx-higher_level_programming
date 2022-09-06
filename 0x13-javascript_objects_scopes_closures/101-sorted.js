87b528df2a2382d065898b7a54911e1fc0535bfa
Asrat Asmelash <asratasse@gmail.com>
2022-09-06 21:05:36 +0300

    0x13. JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
+const dict = require('./101-data').dict;
+const di = {};
+for (const key in dict) {
+  if (di[dict[key]] === undefined) {
+    di[dict[key]] = [];
   }
+  di[dict[key]].push(key);
 }
-console.log(newDict);
+console.log(di);
